c37aede6fd0d3d435865f176dd356136d44a508f
svirom <svirom@yahoo.com>
2016-08-16 15:14:51 +0300

    jQuery

--- js/script.js
+++ js/script.js
@@ -39,15 +39,6 @@ $(window).resize(function() {
 	$('a.nav-opener').removeClass('nav-active');
 });
 
-//delete placeholder from input and textarea on focus
-$('input,textarea').focus(function(){      
-	$(this).data('placeholder',$(this).attr('placeholder'))
-	$(this).attr('placeholder','');
-	});
-	$('input,textarea').blur(function(){
-	$(this).attr('placeholder',$(this).data('placeholder'));
-});
-
 
 });
 
